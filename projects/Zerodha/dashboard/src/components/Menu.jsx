b6d06cf8028224acc9c588bd1b74d15e99bf9357
Piyush6678 <aggarwalpiyush6678@gmail.com>
2025-05-19 19:52:53 +0530

    completes frontend

--- projects/Zerodha/dashboard/src/components/Menu.jsx
+++ projects/Zerodha/dashboard/src/components/Menu.jsx
@@ -1,30 +1,59 @@
 import React ,{useState}  from 'react'
 import { Link } from 'react-router-dom';
 function Menu() {
+
+const [selectedmenu,setSelectedMenu]=useState(0);
+const [isProfileDropdownopen,setIsProfileDropdownopen]=useState(false)
+
+const handleMenuClick=  (index)=>{
+    setSelectedMenu(index);
+}
+const handleProfileClick=  ()=>{
+    setIsProfileDropdownopen(!isProfileDropdownopen);
+}
+
+const menuClass="menu";
+const activemenuClass="menu selected";
+
+
+
     return (<>
     <div className="menu-container">
         <img src="logo.png" alt=""  style={{width:"50px"}} />
         <div className="menus">
             <ul>
                 <li>
-                    <p>Dashboard</p>
+                    <Link  style={{textDecoration:"none"}}to="/"  onClick={handleMenuClick(0)} >
+                    <p   className={selectedmenu==0?activemenuClass:menuClass} > Dashboard</p></Link>
                 </li>
                 <li>
-                    <p>Orders</p>
+                    <Link style={{textDecoration:"none"}}to="/orders"  onClick={handleMenuClick(1)} >
+                       <p   className={selectedmenu==1?activemenuClass:menuClass} >Orders</p>
+                    </Link>
                 </li>
                 <li>
-                    <p>Holding</p>
+                    <Link  style={{textDecoration:"none"}}to="/holdings"  onClick={handleMenuClick(2)}>
+                      <p   className={selectedmenu==2?activemenuClass:menuClass} >Holding</p>
+                </Link>
                 </li>
                 <li>
-                    <p>Position</p>
+                    <Link style={{textDecoration:"none"}}to="/positions"  onClick={handleMenuClick(3)}>
+                       <p   className={selectedmenu==3?activemenuClass:menuClass} >Position</p>   </Link>
                 </li>
                 <li>
-                    <p>Funds</p>
+                    <Link style={{textDecoration:"none"}}to="/funds"  onClick={handleMenuClick(4)} >
+                      <p   className={selectedmenu==4?activemenuClass:menuClass} >Funds</p></Link>
                 </li>
                 <li>
-                    <p>Apps</p>
+                    <Link style={{textDecoration:"none"}}to="/apps"  onClick={handleMenuClick(5)} >
+                       <p   className={selectedmenu==5?activemenuClass:menuClass} >Apps</p> </Link>
                 </li>
             </ul>
+            <hr />
+            <div className="profile"  onClick={handleProfileClick} >
+                <div className="avatar">ZU</div>
+                <p className="username">USERID</p>
+            </div>
         </div>
     </div>
     
--- projects/Zerodha/dashboard/src/components/WatchList.jsx
+++ projects/Zerodha/dashboard/src/components/WatchList.jsx
@@ -1,6 +1,112 @@
-import React from 'react'
+import React,{useState} from 'react'
+
+import {Tooltip,Grow} from "@mui/material"
+import {KeyboardArrowUp,KeyboardArrowDown, BarChartOutlined } from "@mui/icons-material"
+import { watchlist } from '../data/data';
+
 function WatchList() {
-    return (  <></>);
+    return (  <>
+    <div className="watchlist-container">
+        <div className="search-container"><input type="text"
+        id='search'
+        name='search'
+        placeholder='search eg:infy , bse,nifty fut weekly ,gold mcx'
+        className='search'
+        />
+        
+        
+        
+        <span  className='counts' >{watchlist.length}/50</span>
+        
+        
+        </div>
+    
+    <ul className="list">
+
+{watchlist.map((stock,index)=>{
+
+    return(
+<WatchListItem stock={stock}  key={index} />
+)})}
+
+
+    </ul>
+    </div>
+    
+    </>);
 }
 
 export default WatchList;
+
+
+
+const WatchListItem=( {stock} )=>{
+    const [ShowWatchListAction,setShowWatchListAction] =useState(false)
+    const handleMouseEnter=(e)=>{
+        setShowWatchListAction(true)
+    }
+    const handleMouseLeave=(e)=>{
+        setShowWatchListAction(false)
+    }
+    return (
+        <li onMouseEnter={handleMouseEnter}  onMouseLeave={handleMouseLeave}  >
+<div className="item">
+    <p className={stock.isDown?"down":"up"}> {stock.name} </p>
+    <div className='itemiIfo'>
+        <span className='percent' > {stock.percent} </span>
+          {stock.isDown?(
+            <KeyboardArrowDown className="down"/>
+          ) : <KeyboardArrowUp className="up"/>   } 
+          <span className="price"> {stock.price} </span>
+    </div>
+</div>
+{ShowWatchListAction  &&  <WatchListAction uid={stock.name}/> }
+
+        </li>
+    )
+
+
+}
+
+
+const WatchListAction =( {uid} )=>{
+    return (
+        <span className='actions'>
+           <span>
+            <Tooltip  
+            title="Buy (B)"
+            placement='top'  arrow TransitionComponent={Grow}
+            >
+        <button className="buy">But</button>
+
+            </Tooltip>
+            <Tooltip  
+            title="Sell (S)"
+            placement='top'  arrow TransitionComponent={Grow}
+            >
+        <button className="sell">Sell</button>
+
+            </Tooltip>
+            <Tooltip  
+            title="Analytics (A)"
+            placement='top'  arrow TransitionComponent={Grow}
+            >
+
+                <button className="action">
+        <BarChartOutlined className='icon' />
+</button>
+            </Tooltip>
+            <Tooltip  
+            title="More"
+            placement='top'  arrow TransitionComponent={Grow}
+            >
+    <button className="action">
+        <MoreHoriz className='icon' />
+</button>
+
+            </Tooltip>
+            </span> 
+        </span> 
+    )
+
+}
