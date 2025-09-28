0985448dbb6bb5c648dcd332e94c426f5be65e6b
Piyush6678 <aggarwalpiyush6678@gmail.com>
2025-09-29 00:04:38 +0530

    object literal

--- js/oops/app.js
+++ js/oops/app.js
@@ -5,11 +5,13 @@ const user={
      
     getUserDeatils:function (){
 //console.log("Got user details fromdb")
-console.log(`${this.username}`)  
+console.log(`Username:${this.username}`)  
+console.log(this)  // give user details
 }
 
 }
 console.log(user.username);
 console.log(user.getUserDeatils());
+console.log(this)   //{}
 
 
