3388fd96dc6cd3970b72cefe303953e3daa1d6ef
Piyush6678 <aggarwalpiyush6678@gmail.com>
2025-09-30 22:18:55 +0530

    new key word

--- js/oops/object.js
+++ js/oops/object.js
@@ -7,3 +7,21 @@ multiplyby5.power=2
 console.log(multiplyby5(4)) //20
 console.log(multiplyby5.power)  //2
 console.log(multiplyby5.prototype) //{}
+
+function createuser(username,score){
+this.username=username
+this.score=score
+}
+
+
+createuser.prototype.increment=function(){
+    this.score++
+}
+createuser.prototype.printMe=function(){
+    console.log(`score is ${this.score}`)
+}
+
+const chai =new createuser("chai",25)
+const tea =  createuser("tea",150) 
+chai.printMe()
+tea.printMe()// gives error printMe is undefined as new is not used 
