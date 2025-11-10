598ed9e34fddeb7873cf86efda43f7fa78bcc2c7
Piyush6678 <aggarwalpiyush6678@gmail.com>
2025-11-10 06:32:28 +0530

    props

--- react/intro/src/DogCard.jsx
+++ react/intro/src/DogCard.jsx
@@ -1,14 +1,20 @@
 import React from 'react'
-
-export const Image=()=>{
-    return <img src='https://images.squarespace-cdn.com/content/v1/54822a56e4b0b30bd821480c/45ed8ecf-0bb2-4e34-8fcf-624db47c43c8/Golden+Retrievers+dans+pet+care.jpeg'/>
+import Nmae from './name'
+// named export
+export const Image=(props)=>{ 
+    return <img src={props.src}/>
 }
-
-const DogCard = () => {
-  return (
-    <div>
-    <h1>Dog Component</h1>  
-    <Image/>
+// default exports 
+const DogCard = ({name}) => { // destructur props
+    let title = "Dog Component"
+    return (
+        //   pasing jsx as prop 
+        <div>
+            <h1 style={{fontSize:"10px",color:"red"}}  >{title}</h1>
+    <Nmae>
+        <h2> {name}</h2> 
+               </Nmae>   
+    <Image src="https://images.squarespace-cdn.com/content/v1/54822a56e4b0b30bd821480c/45ed8ecf-0bb2-4e34-8fcf-624db47c43c8/Golden+Retrievers+dans+pet+care.jpeg"/>
     
     </div>
   )
