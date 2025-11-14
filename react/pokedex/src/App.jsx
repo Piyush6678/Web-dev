88acdcfc60fc37d39147e74b63b6a752415b5927
Piyush6678 <aggarwalpiyush6678@gmail.com>
2025-11-14 11:59:00 +0530

    style pokemon details

--- react/pokedex/src/App.jsx
+++ react/pokedex/src/App.jsx
@@ -1,15 +1,17 @@
 
-import { BrowserRouter, Route, Routes } from "react-router-dom"
+import { BrowserRouter, Link, Route, Routes } from "react-router-dom"
 import "./App.css"
 
 import CustomRoutes from "./routes/customRoutes"
 function App() {
  
   return (
-    
-
+<div className="outerPokedex" >
+    <Link to={"/"} >
+<h1 id="pokedexheading"> pokedex </h1>
+    </Link>
 <CustomRoutes/>
-
+</div>
 
     
     
--- react/pokedex/src/components/PokemonDetails/PokemonDetails.jsx
+++ react/pokedex/src/components/PokemonDetails/PokemonDetails.jsx
@@ -1,9 +1,48 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import "./Pokemondetails.css"
+import { useParams } from 'react-router-dom'
+import axios from 'axios'
 const PokemonDetails = () => {
+const{id}=useParams()
+const [pokemon,setPokemon]=useState({})
+ async function downloadPokemon(){
+    const response = await axios.get(`https://pokeapi.co/api/v2/pokemon/${id}`)
+
+setPokemon({
+name:response.data.name,
+image:response.data.sprites.other.dream_world.front_default,
+weight:response.data.weight,
+height:response.data.height,
+types:response.data.types.map((t)=>t.type.name)
+})
+
+ } 
+
+ useEffect(()=>{
+downloadPokemon()
+ },[])
+
   return (
-    <div>
-      
+    <div className='pokemonDetailswrapper' >
+       <div className='pokemonDetailsName'  >  
+        
+         {pokemon.name}
+        </div>
+       <div className='pokemonDetailsImage'  >  
+        
+         <img src={pokemon.image} alt={pokemon.name} />
+        </div>
+       <div className='pokemonDetailsweight'  >  
+        
+         weight:{pokemon.weight}
+        </div>
+       <div className='pokemonDetailsHeight'  >  
+        
+         height:{pokemon.height}
+        </div>
+       <div className='pokemonDetailsTypes'  >  
+{ pokemon.types &&   pokemon.types.map((t)=><div key={t.name} > {t} </div>)}
+        </div>
     </div>
   )
 }
