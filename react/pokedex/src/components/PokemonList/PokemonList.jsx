16fd85567d614bb5fdb33bdd57f5fe142a079dc5
Piyush6678 <aggarwalpiyush6678@gmail.com>
2025-11-15 09:48:16 +0530

    manage state variables

--- react/pokedex/src/components/PokemonList/PokemonList.jsx
+++ react/pokedex/src/components/PokemonList/PokemonList.jsx
@@ -3,16 +3,21 @@ import React, { useEffect, useState } from 'react'
 import "./pokemonList.css"
 import Pokemon from '../Pokemon/Pokemon'
 const PokemonList = () => {
-  const [PokedexUrl,setPokedexUrl]=useState("https://pokeapi.co/api/v2/pokemon")
+ 
+ 
 
-  const [nextUrl,setNextUrl]=useState("");
-  const [prevUrl,setPrevUrl]=useState("");
 
-const[ pokemon_list,setPokemon_list]=useState([])
-const [isLoading,setIsLoading]=useState(true)
+const [pokemonListState,setPokemonListState]=useState({
+pokedexUrl:"https://pokeapi.co/api/v2/pokemon",
+nextUrl:"",
+prevUrl:"",
+pokemonList:[],
+isLoading:true
+})
+
     useEffect(async()=>{
-        setIsLoading(true)
-const response=await axios.get(PokedexUrl)
+        setPokemonListState((prev)=>  ({...prev,isLoading:true}))
+const response=await axios.get(pokemonListState.pokedexUrl)
 const pokemonResults=response.data.results;
 const pokemonResultPromise=pokemonResults.map(async(pokemon)=> axios.get(pokemon.url))
 const pokemonData=await axios.all(pokemonResultPromise)
@@ -24,21 +29,24 @@ const res=(pokemonData.map((pokeData)=>{
         id:pokemon.id
      }
 }))
+setPokemonListState(prev => ({
+  ...prev,
+  nextUrl: response.data.next,
+  prevUrl: response.data.previous,
+  pokemonList: res,
+  isLoading: false
+}));
 
-setNextUrl(response.data.next)
-setPrevUrl(response.data.previous)
 
-setPokemon_list(res)
-setIsLoading(false);
-    },[])
+    },[pokemonListState.pokedexUrl])
 
     
     return (
     <div className='PokemonList'>
   
    <div className="pokemonWrapper">
-       {(isLoading)?"Loading...":(
-            pokemon_list.map((p)=><Pokemon key={p.id} id={p.id} name={p.name} image={p.image
+       {(pokemonListState.isLoading)?"Loading...":(
+            pokemonListState.pokemonList.map((p)=><Pokemon key={p.id} id={p.id} name={p.name} image={p.image
             } />)
 
 
@@ -48,14 +56,14 @@ setIsLoading(false);
 
 
 <div className='controls' >
-    <button disabled={(prevUrl==null)}  onClick= {  ()=>{
-        setPokedexUrl(prevUrl)
+    <button disabled={(pokemonListState.prevUrl==null)}  onClick= {  ()=>{
+        setPokemonListState((prev)=>({...prev,pokedexUrl:pokemonListState.prevUrl}))
     }    } >
     Previous
     </button>
     <button  onClick= {  ()=>{
-        setPokedexUrl(nextUrl)
-    }    }  disabled={(nextUrl==null)}>
+        setPokemonListState((prev)=>({...prev,pokedexUrl:pokemonListState.nextUrl}))
+    }    }  disabled={(pokemonListState.nextUrl==null)}>
    Next
     </button>
     </div>
