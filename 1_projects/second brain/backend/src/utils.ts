e18bf68d5c133687cca0c53c95ad98a66f0e1087
Piyush6678 <aggarwalpiyush6678@gmail.com>
2026-01-05 08:17:06 +0530

    Moved second brain to its own repository

--- 1_projects/second brain/backend/src/utils.ts
+++
@@ -1,8 +0,0 @@
-export function random(len:number){
-    let result = '';
-    const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
-    for (let i = 0; i < len; i++) {
-        result += characters.charAt(Math.floor(Math.random() * characters.length));
-    }
-    return result;
-}
--- 1_projects/second brain/frontend/src/icons/iconstlyng.tsx
+++
@@ -1,7 +0,0 @@
-export interface Iconstyle{
-    size:"sm"| "md"|"lg";
-}
-export const iconsize={
-    "sm":"size-2",
-    "md":"size-5",
-    "lg":"size-6",}
