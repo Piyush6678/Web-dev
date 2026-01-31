5d536455019c0c894816050497614592450861a1
Piyush6678 <aggarwalpiyush6678@gmail.com>
2026-01-31 21:07:14 +0530

    prettify the ui

--- next.js/intro/app/users/page.tsx
+++ next.js/intro/app/users/page.tsx
@@ -9,9 +9,16 @@ export default async function Home() {
   const userData = await getUserDetails();
 
   return (
-    <div>
-      {userData.email}
-      {userData.name}
+    <div className="flex flex-col justify-center h-screen">
+        <div className="flex justify-center">
+            <div className="border p-8 rounded">
+                <div>
+                    Name: {userData?.name}
+                </div>
+                
+                {userData?.email}
+            </div>
+        </div>
     </div>
   );
 }
